dd2cd7cb8e48ce6a11dbed2608de668d9060e181
fernnx2 <fernandoaguilera_@outlook.es>
2018-06-08 20:03:05 -0600

    se cambiaron los destinos de services

---
+++ resources/MarcaResourceClient.js
@@ -0,0 +1,19 @@
+import AbstractResourceClient from './AbstractResourceClient.js';
+class MarcaResourceClient extends AbstractResourceClient{
+
+    constructor(){
+        super();
+        this.url += "marca";
+
+    }
+
+    findAllMarca() {
+        return fetch(this.url);
+    }
+    findByName(nombre){
+        return fetch(this.url+"marca/marca" +nombre);
+    }
+
+}
+
+export default MarcaResourceClient;
