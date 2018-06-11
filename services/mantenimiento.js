9250dedcf412bb3f4cbc2bae1fb0d16379f2e9e5
fernnx2 <fernandoaguilera_@outlook.es>
2018-06-11 09:16:24 -0600

    se programo la vista para los mantenimientos no terminados

--- services/mantenimiento.js
+++ services/mantenimiento.js
@@ -3,7 +3,6 @@ import e from '../controlador/EstadoDetalleMantenimientoController.js';
 
 var mantenimiento = angular.module('mantenimiento',[]);
 mantenimiento.controller('data', function($scope){
-    $scope.datos = [{"fechaInicio":"23132","responsable":"tsdf","tipo":"ds","fechaFin":"sds"},{"fechaInicio":"23132","responsable":"tsdf","tipo":"ds","fechaFin":"sds"}];
     $scope.rest;
     let mantenimietosTerminados = e.findMantenimientosTerminados();
     mantenimietosTerminados.then(function(response){
@@ -14,3 +13,16 @@ mantenimiento.controller('data', function($scope){
     });
     
 });
+
+mantenimiento.controller('noterminados', function($scope){
+    $scope.rest;
+    let mantenimietosNoTerminados = e.findMantenimientosNoTerminados();
+    mantenimietosNoTerminados.then(function(response){
+        $scope.$apply(function(){
+            $scope.rest = response;
+            console.log(response);
+        });
+    });
+    
+});
+
